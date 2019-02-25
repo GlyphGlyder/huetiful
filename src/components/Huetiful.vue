bf3c66ed5f167b828428fb4d86e5f0c9785c86b4
Bjorkbat <bjorkbat@gmail.com>
2019-02-24 23:12:19 -0500

    Trimmed this guy a bit, removed all the methods

---
+++ src/components/Huetiful.vue
@@ -0,0 +1,44 @@
+<!-- Huetiful itself.  Juggles between rendering two components.  The window,
+  which is effectively a rounded square, and the panel, which is where all the
+  action happens -->
+<template>
+  <div id="Huetiful">
+
+    <hue-window
+      v-if="minimized"
+      v-on:click="minimized = false"
+      v-bind:minimized="minimized"
+      v-bind:red="red"
+      v-bind:green="green"
+      v-bind:blue="blue">
+    </hue-window>
+
+    <hue-panel
+      v-if="!minimized"
+      v-on:close="$emit('close')"
+      v-on:minimize="minimized = true"
+      v-on:red="red = $event"
+      v-on:green="green = $event"
+      v-on:blue="blue = $event"
+      v-bind:red="red"
+      v-bind:green="green"
+      v-bind:blue="blue">
+    </hue-panel>
+
+  </div>
+</template>
+
+<script>
+import HuePanel from './subcomponents/HuePanel.vue';
+import HueWindow from './subcomponents/HueWindow.vue';
+export default {
+  name: 'Huetiful',
+  components: { HuePanel, HueWindow },
+  props: ['red', 'green', 'blue'],
+  data: function() {
+    return {
+      minimized: true
+    }
+  }
+}
+</script>
