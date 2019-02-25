b5bafbf62803b6afb344c705f76deaaf8bdd943d
Bjorkbat <bjorkbat@gmail.com>
2019-02-25 07:57:01 -0500

    Added palette wells

---
+++ src/components/subcomponents/HuePaletteWell.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hue-palette-well"
+    v-bind:style="{'background-color': colorRGBA}"
+    v-on:click="setColor">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HuePaletteWell",
+  props: ['color'],
+
+  computed: {
+
+    /**
+     * Returns color as an rgba string
+     */
+    colorRGBA: function() {
+      return "rgba(" + this.color.red + ", " + this.color.green + ", " + this.color.blue + ", " + 100 + ")";
+    }
+
+  },
+
+  methods: {
+
+    setColor: function() {
+      this.$emit('setColor', this.color);
+    }
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hue-palette-well {
+  border-radius: 4px;
+  height: 40px;
+  width: 40px;
+  margin: 4px;
+  cursor: pointer;
+}
+</style>
