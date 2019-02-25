ee6782b0a4456bffd1f2a83d85b797d5662aa59d
Bjorkbat <bjorkbat@gmail.com>
2019-02-24 23:12:04 -0500

    Removed fill

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="Demo">
+    <huetiful
+      v-bind:red="red"
+      v-bind:green="green"
+      v-bind:blue="blue"
+      v-on:red="changeRed"
+      v-on:green="changeGreen"
+      v-on:blue="changeBlue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Huetiful from './components/Huetiful.vue';
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'Demo',
+  components: { Huetiful },
+  data: function() {
+    return {
+      red: 255,
+      green: 0,
+      blue: 0
+    }
+  },
+
+  methods: {
+
+    changeRed: function(red) {
+      this.red = red;
+    },
+
+    changeGreen: function(green) {
+      this.green = green;
+    },
+
+    changeBlue: function(blue) {
+      this.blue = blue;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
