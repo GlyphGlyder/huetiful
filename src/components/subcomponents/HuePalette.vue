e1c9e50b1e7d748d02ced9116af9d1db6053d31f
Bjorkbat <bjorkbat@gmail.com>
2019-02-25 08:01:28 -0500

    Going to go ahead and add these

---
+++ src/components/subcomponents/HuePalette.vue
@@ -0,0 +1,253 @@
+<template>
+  <div id="HuePalette" class="hue-palette">
+
+    <div>
+      <hue-palette-well v-for="color in colors.setOne"
+        v-bind:color="color"
+        v-on:setColor="setColor">
+      </hue-palette-well>
+    </div>
+
+    <div>
+      <hue-palette-well v-for="color in colors.setTwo"
+        v-bind:color="color"
+        v-on:setColor="setColor">
+      </hue-palette-well>
+    </div>
+
+    <div>
+      <hue-palette-well v-for="color in colors.setThree"
+        v-bind:color="color"
+        v-on:setColor="setColor">
+      </hue-palette-well>
+    </div>
+
+    <div>
+      <hue-palette-well v-for="color in colors.setFour"
+        v-bind:color="color"
+        v-on:setColor="setColor">
+      </hue-palette-well>
+    </div>
+
+    <div>
+      <hue-palette-well v-for="color in colors.setFive"
+        v-bind:color="color"
+        v-on:setColor="setColor">
+      </hue-palette-well>
+    </div>
+
+    <div>
+      <hue-palette-well v-for="color in colors.setSix"
+        v-bind:color="color"
+        v-on:setColor="setColor">
+      </hue-palette-well>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import HuePaletteWell from './HuePaletteWell.vue';
+export default {
+  name: "HuePalette",
+  components: { HuePaletteWell },
+
+  data: function() {
+    return {
+      colors: {
+        setOne: [
+          {
+            red: 255,
+            green: 122,
+            blue: 92
+          },
+          {
+            red: 255,
+            green: 84,
+            blue: 46,
+          },
+          {
+            red: 255,
+            green: 46,
+            blue: 0
+          },
+          {
+            red: 209,
+            green: 38,
+            blue: 0
+          },
+          {
+            red: 163,
+            green: 30,
+            blue: 0
+          }
+        ],
+        setTwo: [
+          {
+            red: 255,
+            green: 195,
+            blue: 117
+          },
+          {
+            red: 255,
+            green: 178,
+            blue: 78
+          },
+          {
+            red: 255,
+            green: 162,
+            blue: 39
+          },
+          {
+            red: 209,
+            green: 133,
+            blue: 32
+          },
+          {
+            red: 163,
+            green: 104,
+            blue: 25
+          }
+        ],
+        setThree: [
+          {
+            red: 255,
+            green: 225,
+            blue: 92
+          },
+          {
+            red: 255,
+            green: 216,
+            blue: 46
+          },
+          {
+            red: 255,
+            green: 208,
+            blue: 0
+          },
+          {
+            red: 209,
+            green: 171,
+            blue: 0,
+          },
+          {
+            red: 163,
+            green: 133,
+            blue: 0
+          }
+        ],
+        setFour: [
+          {
+            red: 92,
+            green: 236,
+            blue: 133
+          },
+          {
+            red: 46,
+            green: 231,
+            blue: 98
+          },
+          {
+            red: 0,
+            green: 226,
+            blue: 64
+          },
+          {
+            red: 0,
+            green: 185,
+            blue: 53
+          },
+          {
+            red: 0,
+            green: 144,
+            blue: 41
+          }
+        ],
+        setFive: [
+          {
+            red: 115,
+            green: 199,
+            blue: 253
+          },
+          {
+            red: 75,
+            green: 183,
+            blue: 252
+          },
+          {
+            red: 35,
+            green: 168,
+            blue: 252
+          },
+          {
+            red: 29,
+            green: 138,
+            blue: 207
+          },
+          {
+            red: 23,
+            green: 107,
+            blue: 161
+          }
+        ],
+        setSix: [
+          {
+            red: 162,
+            green: 92,
+            blue: 255
+          },
+          {
+            red: 135,
+            green: 46,
+            blue: 255
+          },
+          {
+            red: 109,
+            green: 0,
+            blue: 255
+          },
+          {
+            red: 90,
+            green: 0,
+            blue: 209
+          },
+          {
+            red: 70,
+            green: 0,
+            blue: 163
+          }
+        ]
+      }
+    }
+  },
+
+  methods: {
+
+    setColor: function(color) {
+      this.$emit('setColor', color);
+    }
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hue-palette {
+  position: relative;
+  border: 2px solid #333;
+  border-radius: 10px;
+  box-shadow:
+    0 0 1px #BBB,
+    2px 2px 2px #BBB;
+  padding: 10px;
+  background-color: #FFF;
+  margin-left: 10px;
+
+  & > div {
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 100%;
+  }
+}
+</style>
---
+++ src/components/subcomponents/HueWindow.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    id="HueWindow"
+    class="huetiful-window"
+    v-bind:class="{minimized, bordered}"
+    v-bind:style="{'background-color': color}"
+    v-on:click="onClick">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HueWindow",
+  props: ['minimized', 'red', 'green', 'blue'],
+
+  computed: {
+
+    // Computed property, determines if a colored border should be added to
+    // help window contrast better against background
+    bordered: function() {
+
+      if (this.red > 250 & this.green > 250 & this.blue > 250) {
+        return true;
+      }
+      return false;
+    },
+
+    // Defines the color present on the window at a given moment
+    color: function() {
+
+      return "rgba(" + this.red + "," + this.green + ", " + this.blue + ", " + 100 + ")";
+
+    }
+
+  },
+
+  methods: {
+
+    onClick: function() {
+      this.$emit('click');
+    }
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.huetiful-window {
+  background-color: #FF0000;
+  border-radius: 100%;
+  border: 1px solid transparent;
+  box-shadow: 0 2px 2px #AAA;
+  cursor: pointer;
+  height: 100px;
+  width: 100px;
+  transition: border 0.4s ease;
+
+  &.minimized {
+    border-radius: 8px;
+    width: 40px;
+    height: 40px;
+  }
+
+  &.bordered {
+    border: 1px solid #CCC;
+  }
+}
+</style>
